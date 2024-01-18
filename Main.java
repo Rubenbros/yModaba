f11944ee76b0b6048c60f8d41d09634a6acba43f
Rubenbros <rubenbros@hotmail.es>
2024-01-18 17:10:56 +0100

    Primera versión

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número: ");
+        int n = scanner.nextInt();
+
+        String resultado = "y modaba";
+
+        for (int i = 1; i <= n; i++) {
+            resultado += "a";
+            System.out.println(resultado);
+        }
     }
 }
